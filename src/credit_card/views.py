da831f3d93fffdf29924bd40df9c1b323e7da1d2
guilherme <guilherme@pop-os.localdomain>
2023-08-30 02:28:18 -0300

    inject auth dependency on credit card routers

--- src/credit_card/views.py
+++ src/credit_card/views.py
@@ -1,35 +1,44 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Path, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
 from database.database import get_db
-from src.credit_card.exceptions import CreditCardNotFound
 from src.credit_card.repository import CreditCardRepository
 from src.credit_card.schemas import CreditCardCreateSchema
 from src.credit_card.utils import CreditCardHelper
+from src.credit_card.exceptions import CreditCardNotFound
+from src.user.utils import get_current_user
 
 router = APIRouter()
 
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
 @router.get("/")
-async def fetch_all_credit_cards(db: Session = Depends(get_db)):
-    credit_cards = CreditCardRepository().get_all(db=db)
+async def fetch_all_credit_cards(user: user_dependency, db: Session = Depends(get_db)):
+    repository = CreditCardRepository()
+    credit_cards = repository.get_all(db=db)
     return credit_cards
 
 
 @router.get("/{credit_card_id}")
 async def fetch_credit_card_by_id(
+        user: user_dependency,
         credit_card_id: Annotated[int, Path(title="The ID of the credit card to get")],
         db: Session = Depends(get_db)
 ):
-
-    credit_card = CreditCardRepository().get_by_id(db=db, _id=credit_card_id)
+    repository = CreditCardRepository()
+    credit_card = repository.get_by_id(db=db, _id=credit_card_id)
     if not credit_card:
         raise CreditCardNotFound(id=credit_card_id)
     return credit_card
 
 @router.post("/", status_code=201)
-async def create_credit_card(body: CreditCardCreateSchema, db: Session = Depends(get_db)):
+async def create_credit_card(user: user_dependency,
+                             body: CreditCardCreateSchema,
+                             db: Session = Depends(get_db)
+                             ):
     custom_date = CreditCardHelper.parse_date(date=body.exp_date)
     repository = CreditCardRepository()
     credit_card = await repository.create(db=db, credit_card_schema=body, date=custom_date)
