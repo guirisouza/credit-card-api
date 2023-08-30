20759adc333efaccfb50ab6028b1262b5ca9c526
guilherme <guilherme@pop-os.localdomain>
2023-08-30 02:31:59 -0300

    typing returns

--- src/user/repository.py
+++ src/user/repository.py
@@ -6,13 +6,13 @@ from src.user.models import UsersModel
 
 class UserRepository:
 
-    async def create(self, db: Session, data: CreateUserInputSchema):
+    async def create(self, db: Session, data: CreateUserInputSchema) -> UsersModel:
         user = UsersModel(username=data.username, password=data.password)
         db.add(user)
         db.commit()
         db.refresh(user)
         return user
 
-    def get_user_by_username(self, username: str, db: Session):
+    def get_user_by_username(self, username: str, db: Session) -> UsersModel:
         user = db.query(UsersModel).filter(UsersModel.username == username).first()
         return user
