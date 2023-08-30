28d54e44357c6ebd09da0bdd2b98244202525e1d
guilherme <guilherme@pop-os.localdomain>
2023-08-30 02:14:44 -0300

    fix types and lint

--- src/credit_card/repository.py
+++ src/credit_card/repository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from src.credit_card.models import CreditCardModel
@@ -6,7 +8,7 @@ from src.credit_card.schemas import CreditCardCreateSchema
 #TODO: fix size of the lines
 # Add the returning ype
 class CreditCardRepository:
-    async def create(self, db: Session, credit_card_schema: CreditCardCreateSchema, date):
+    async def create(self, db: Session, credit_card_schema: CreditCardCreateSchema, date) -> CreditCardModel:
         if credit_card_schema.cvv:
             credit_card_schema.cvv = None
         db_credit_card = CreditCardModel(
@@ -20,8 +22,8 @@ class CreditCardRepository:
         db.refresh(db_credit_card)
         return db_credit_card
 
-    def get_by_id(self, db: Session, _id):
+    def get_by_id(self, db: Session, _id) -> CreditCardModel:
         return db.query(CreditCardModel).filter(CreditCardModel.id == _id).first()
 
-    def get_all(self, db: Session):
+    def get_all(self, db: Session) -> List[CreditCardModel]:
         return db.query(CreditCardModel).all()
