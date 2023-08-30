a54bdfd74449650ff1df631dfae04e12afed0d18
guilherme <guilherme@pop-os.localdomain>
2023-08-30 02:34:15 -0300

    creating prefix routes

---
+++ src/router.py
@@ -0,0 +1,17 @@
+from fastapi.routing import APIRouter
+from src import credit_card
+from src import user
+
+api_router = APIRouter()
+
+api_router.include_router(
+    user.router,
+    prefix="/auth",
+    tags=["Authentication"],
+)
+
+api_router.include_router(
+    credit_card.router,
+    prefix="/credit-card",
+    tags=["Credit Card"],
+)
